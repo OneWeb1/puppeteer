87e29315c4959e74c98d2dbe2f5be774a2bd75ef
OneWeb1 <alicksadler18@gmail.com>
2023-09-26 13:07:49 +0300

    working

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 const app = require('express')()
 
 let chrome = {}
-let puppeteer
+let puppeteer = require('puppeteer')
 
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-	chrome = require('chrome-aws-lambda')
-	puppeteer = require('puppeteer-core')
-} else {
-	puppeteer = require('puppeteer')
-}
+// if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+// 	chrome = require('chrome-aws-lambda')
+// 	puppeteer = require('puppeteer-core')
+// } else {
+// 	puppeteer = require('puppeteer')
+// }
 
 app.get('/api', async (req, res) => {
 	let options = {}
-	if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-		options = {
-			args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
-			defaultViewport: chrome.defaultViewport,
-			executablePath: await chrome.executablePath,
-			headless: true,
-			ignoreHTTPSErrors: true,
-		}
+	// if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+	options = {
+		args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+		defaultViewport: chrome.defaultViewport,
+		executablePath: await chrome.executablePath,
+		headless: true,
+		ignoreHTTPSErrors: true,
 	}
+	// }
 
 	try {
 		let browser = await puppeteer.launch(options)
 		let page = await browser.newPage()
 		await page.goto('https://www.google.com')
+		console.log(process.env.AWS_LAMBDA_FUNCTION_VERSION)
 		res.send(await page.title())
 	} catch (err) {
 		console.error(err)
